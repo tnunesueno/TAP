a19f06e0debf796bf9c42b68dc2f0a0746643a27
tnunesueno <tnunesueno25@scienceleadership.org>
2025-07-04 12:42:04 -0400

    fixed some shit in sidebar

--- my-app/src/Sidebar.js
+++ my-app/src/Sidebar.js
@@ -1,16 +1,28 @@
+import BlogCard from './BlogCard';
+import blogPosts from './blogPosts';
+
 function Sidebar() {
+
+  const posts = blogPosts;
+  const post = posts[0]; 
+
   return (
     <div className="rightcolumn">
       <div className="card">
-        <h2>About Me</h2>
+        <h2>About Us</h2>
         {/* <div className="fakeimg" style={{ height: 200 }}>Image</div> */}
         <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
       </div>
       <div className="card">
         <h3>Popular Post</h3>
-        <div className="fakeimg">Image</div><br />
-        <div className="fakeimg">Image</div><br />
-        <div className="fakeimg">Image</div>
+        <BlogCard
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              description={post.description}
+              date={post.date}
+              text={post.text}
+            />
       </div>
     </div>
   );
